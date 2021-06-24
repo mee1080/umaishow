0effe8a3b9d0d5e241e13b068354095b2d736607
Nishino <m-nishino@tstaff.jp>
2021-06-24 18:02:00 +0900

    Tutorial

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("multiplatform") version "1.5.10"
+    id("org.jetbrains.compose") version "0.5.0-build226"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+kotlin {
+    /* Targets configuration omitted. 
+    *  To find out how to configure the targets, please follow the link:
+    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+            }
+        }
+    }
+//    sourceSets {
+//        val commonMain by getting {
+//            dependencies {
+//                implementation(kotlin("stdlib-common"))
+//            }
+//        }
+//        val commonTest by getting {
+//            dependencies {
+//                implementation(kotlin("test-common"))
+//                implementation(kotlin("test-annotations-common"))
+//            }
+//        }
+//    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "umaishow"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
